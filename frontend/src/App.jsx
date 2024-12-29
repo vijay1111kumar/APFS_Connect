2fa87b4c4eaad732ff4bdc26bb4bccc6a48b1e8a
vijay1111kumar <vijaykumarg@loan2wheels.com>
2024-12-30 01:46:32 +0530

    Routes for PromotionDetailPage and UserDetialsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
-import UserDetailsPage from "./pages/UserDetailsPage";
 import FlowStatsPage from "./pages/FlowStatsPage";
 import PromotionsPage from "./pages/PromotionsPage";
 import RemaindersPage from "./pages/RemaindersPage";
@@ -9,6 +8,8 @@ import TimeBasedPage from "./pages/TimeBasedPage";
 import ErrorAnalyticsPage from "./pages/ErrorAnalyticsPage";
 import SummaryPage from "./pages/SummaryPage";
 import TestPage from "./pages/TestPage";
+import PromotionsDetailsPage from "./pages/PromotionsDetailsPage";
+import UserDetailsPage from "./pages/UserDetailsPage";
 
 const App = () => {
   return (
@@ -17,12 +18,13 @@ const App = () => {
         <Route path="/" element={<DashboardPage />} />
         <Route path="/promotions" element={<PromotionsPage />} />
         <Route path="/remainders" element={<RemaindersPage />} />
-        <Route path="/user-stats" element={<UserDetailsPage />} />
         <Route path="/flow-stats" element={<FlowStatsPage />} />
         <Route path="/time-based" element={<TimeBasedPage />} />;
         <Route path="/error-analytics" element={<ErrorAnalyticsPage />} />;
         <Route path="/summary" element={<SummaryPage />} />;
         <Route path="/test" element={<TestPage />} />;
+        <Route path="/promotions/:id" element={<PromotionsDetailsPage />} /> 
+        <Route path="/users/:id" element={<UserDetailsPage />} />
       </Routes>
     </Router>
   );
