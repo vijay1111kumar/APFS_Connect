56147158c6257fecfb6da3e6951d827ca4c330ee
vijay1111kumar <vijaykumarg@loan2wheels.com>
2025-01-06 18:04:32 +0530

    New routes for promotions and remainders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import FlowStatsPage from "./pages/FlowStatsPage";
-import PromotionsPage from "./pages/PromotionsPage";
-import RemaindersPage from "./pages/RemaindersPage";
 import TimeBasedPage from "./pages/TimeBasedPage";
 import ErrorAnalyticsPage from "./pages/ErrorAnalyticsPage";
 import SummaryPage from "./pages/SummaryPage";
 import TestPage from "./pages/TestPage";
-import PromotionsDetailsPage from "./pages/PromotionsDetailsPage";
 import UserDetailsPage from "./pages/UserDetailsPage";
+
+import PromotionsPage from "./pages/PromotionsPage";
+import PromotionsDetailsPage from "./pages/PromotionsDetailsPage";
+import PromotionUserDetailsPage from "./pages/PromotionUserDetailsPage";
+
+import RemaindersPage from "./pages/RemaindersPage";
+import RemaindersUserDetailsPage from "./pages/RemaindersUserDetailsPage";
+import RemaindersDetailsPage from "./pages/RemaindersDetailsPage";
 
 const App = () => {
   return (
@@ -18,12 +23,15 @@ const App = () => {
         <Route path="/" element={<DashboardPage />} />
         <Route path="/promotions" element={<PromotionsPage />} />
         <Route path="/remainders" element={<RemaindersPage />} />
-        <Route path="/flow-stats" element={<FlowStatsPage />} />
-        <Route path="/time-based" element={<TimeBasedPage />} />;
-        <Route path="/error-analytics" element={<ErrorAnalyticsPage />} />;
-        <Route path="/summary" element={<SummaryPage />} />;
-        <Route path="/test" element={<TestPage />} />;
+        {/* <Route path="/flow-stats" element={<FlowStatsPage />} /> */}
+        {/* <Route path="/time-based" element={<TimeBasedPage />} />; */}
+        {/* <Route path="/error-analytics" element={<ErrorAnalyticsPage />} />; */}
+        {/* <Route path="/summary" element={<SummaryPage />} />; */}
+        {/* <Route path="/test" element={<TestPage />} />; */}
         <Route path="/promotions/:id" element={<PromotionsDetailsPage />} /> 
+        <Route path="/promotions/users/:id" element={<PromotionUserDetailsPage />} />
+        <Route path="/remainders/:id" element={<RemaindersDetailsPage />} /> 
+        <Route path="/remainders/users/:id" element={<RemaindersUserDetailsPage />} />
         <Route path="/users/:id" element={<UserDetailsPage />} />
       </Routes>
     </Router>
