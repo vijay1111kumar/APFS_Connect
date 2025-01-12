b543813e3fe6d0056aeecbaa0f6940152215125f
vijay1111kumar <vijaykumarg@loan2wheels.com>
2025-01-12 16:47:55 +0530

    improved api functions and added camapign api functions

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://apfs.loan2wheels.com/apfsconnect/api/analytics', 
+  baseURL: 'https://apfs.loan2wheels.com/apfsconnect/api', 
 });
 
 export const fetchOverview = () => apiClient.get('/overview');
@@ -24,10 +24,31 @@ export const fetchDashboardMetrics = async (category) => {
   }
 };
 
+export const fetchFlows = async () => {
+  try {
+    const { data } = await apiClient.get('/flows');
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching flows:", error);
+    throw error;
+  }
+};
+
+export const createPromotion = async (requestBody) => {
+  try {
+    const { data } = await apiClient.post('/promotions', requestBody);
+    return data.data;
+  } catch (error) {
+    console.error("Error creating promotions:", error);
+    throw error;
+  }
+};
+
+
 export const fetchPromotions = async () => {
   try {
     const { data } = await apiClient.get('/promotions');
-    return data;
+    return data.data;
   } catch (error) {
     console.error("Error fetching promotions:", error);
     throw error;
@@ -37,12 +58,80 @@ export const fetchPromotions = async () => {
 export const fetchPromotionDetails = async (id) => {
   try {
     const { data } = await apiClient.get(`/promotions/${id}`);
-    return data;
+    return data.data;
   } catch (error) {
     console.error("Failed to fetch promotion details");
     throw error;
   }
 };
+
+export const fetchCampaignsForPromotion = async (id) => {
+  try {
+    const { data } = await apiClient.get(`promotions/${id}/campaigns`)
+    return data.data;
+  } catch (error) {
+    console.error("Failed to fetch campaigns for promotion");
+    throw error;
+  }
+};
+
+export const fetchCampaignMetrics = async (campaignId) => {
+  try {
+    const { data } = await apiClient.get(`/campaigns/${campaignId}/metrics`);
+    return data.data;
+  } catch (error) {
+      console.error("Failed to fetch campaigns for promotion");
+      throw error;
+  }
+};
+
+export const fetchPromotionsPerformance = async () => {
+  try {
+    const { data } = await apiClient.get('/promotions?performance');
+    console.log(data.data.Promotion)
+    return data.data.Promotion;
+} catch (error) {
+    console.error("Failed to fetch campaigns for promotion");
+    throw error;
+}
+};
+
+
+// --------------------------------------------
+
+export const createCampaigns = async (requestBody) => {
+  try {
+    const { data } = await apiClient.post('/campaigns', requestBody);
+    return data
+  } catch (error) {
+    console.error("Error creating campaigns:", error);
+    throw error;
+  }
+};
+
+
+export const fetchCampaigns = async () => {
+  try {
+    const { data } = await apiClient.get('/campaigns');
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching campaigns:", error);
+    throw error;
+  }
+};
+
+
+export const fetchCampaignsPerformance = async () => {
+  try {
+    const { data } = await apiClient.get('/campaigns?performance');
+    return data.data.Campaign;
+} catch (error) {
+    console.error("Failed to fetch campaigns for Campaign");
+    throw error;
+}
+};
+
+
 
 export const fetchPromotionUsers = async (id) => {
   try {
@@ -58,7 +147,7 @@ export const fetchPromotionUsers = async (id) => {
 export const fetchRemainders = async () => {
   try {
     const { data } = await apiClient.get('/remainders');
-    return data;
+    return data.data;
   } catch (error) {
     console.error("Error fetching remainders:", error);
     throw error;
@@ -92,6 +181,30 @@ export const fetchUserDetails = async (id) => {
     return data;
   } catch (error) {
     console.error("Failed to fetch users details");
+    throw error;
+  }
+};
+
+
+// Upload Function
+// -----------------------------------------------
+
+export const uploadFile = async (file) => {
+  try {
+
+    console.log("Uploadingn ", file.type)
+
+    // Prepare the file for upload
+    const { data } = await apiClient.post("/upload", file, {
+      headers: {
+        "Content-Type": file.type || "application/octet-stream",
+        "X-File-Name": file.name
+      },
+    });
+
+    return data.data; // Return the response data
+  } catch (error) {
+    console.error("File upload failed:", error);
     throw error;
   }
 };
