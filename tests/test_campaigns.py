b8c37345fe2fe9f1e1e3ee92010d026a2bab6f41
vijay1111kumar <vijaykumarg@loan2wheels.com>
2025-01-15 12:03:37 +0530

    Test for campaigns

--- tests/test_campaigns.py
+++ tests/test_campaigns.py
@@ -1,36 +1,37 @@
 import sys
-sys.path.append("../")
-
-from datetime import datetime, timedelta
-from batch_processor.scheduler import CampaignScheduler
-
-campaign_scheduler = CampaignScheduler()
-campaign_scheduler.start()
-
-# Schedule a Remainder
-campaign_scheduler.schedule_campaign(
-    campaign_id="campaign_123",
-    user_id="user_001",
-    message="This is your remainder!",
-    schedule_time=datetime.now() + timedelta(seconds=10),
-    activity_type="REMAINDER",
-    excel_file="../upload/campaign_test_excel.xlsx"
-)
-
-# Schedule a Promotion
-# campaign_scheduler.schedule_campaign(
-#     campaign_id="campaign_456",
-#     user_id=None,  # Not needed for promotions
-#     message=None,  # Not needed for promotions
-#     schedule_time=datetime.now() + timedelta(seconds=30),
-#     activity_type="PROMOTION",
-#     excel_file="../upload/campaign_test_excel.xlsx"
-# )
-
 import time
 
-try:
-    while True:
-        time.sleep(1)
-except (KeyboardInterrupt, SystemExit):
-    campaign_scheduler.stop()
+from datetime import datetime, timedelta
+
+sys.path.append("../")
+from utils.scheduler import campaign_scheduler
+
+# Schedule a Remainder Campaign
+remainder_campaign_data = {
+    "campaign_id" :"campaign_123",
+    "schedule_time" :datetime.now() + timedelta(seconds=60),
+    "activity_type" :"REMAINDER",
+    "excel_file" :"../upload/campaign_test_excel.xlsx",
+    "retry_attempts" :3,
+    "retry_interval" :1
+}
+
+campaign_scheduler.schedule_campaign(
+    campaign_data=remainder_campaign_data,
+    user_id="APFS0001",
+)
+
+remainder_campaign_data = {
+    "campaign_id" :"campaign_456",
+    "schedule_time" :datetime.now() + timedelta(seconds=60),
+    "activity_type" :"PROMOTION",
+    "excel_file" :"../upload/campaign_test_excel.xlsx",
+    "retry_attempts" :3,
+    "retry_interval" :1
+}
+
+# Schedule a Promotion Campaign
+campaign_scheduler.schedule_campaign(
+    campaign_data=remainder_campaign_data,
+    user_id="APFS0001",  
+)
