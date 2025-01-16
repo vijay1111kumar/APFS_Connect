db340f38702a3ca095fb755d3c0627e247785f23
vijay1111kumar <vijaykumarg@loan2wheels.com>
2025-01-16 13:03:08 +0530

    Migrated Global Registry to Redis, still need to improve a lot

--- registries/global_registry.py
+++ registries/global_registry.py
@@ -1,26 +1,25 @@
 import os
-import sys
 import json
+import redis
 import importlib.util
-
 from typing import Dict, Optional, Any, List, Callable
 
-# sys.path.append("../")
 from utils.logger import LogManager
 from utils.file_handler import read_json_file, update_json_file
 
 PROCESSOR_DIR = "processors"
 FLOW_DIR = "flows"
 ARCHIVE_DIR = "../archive/flows"
+
 log_manager = LogManager()
 logger = log_manager.get_logger("global_registry")
 
-def get_functions_from_script(script_path: str) -> Dict[str, Callable]:
 
+def get_functions_from_script(script_path: str) -> Dict[str, Callable]:
     functions = {}
     if not os.path.exists(script_path):
         return functions
-    
+
     module_name = os.path.splitext(os.path.basename(script_path))[0]
     try:
         spec = importlib.util.spec_from_file_location(module_name, script_path)
@@ -36,25 +35,41 @@ def get_functions_from_script(script_path: str) -> Dict[str, Callable]:
 
     return functions
 
+
 class GlobalRegistry:
-    def __init__(self) -> None:
-        self.flows: Dict[str, Dict] = {}
-        self.triggers: Dict[str, str] = {}
-        self.flow_processors: Dict[str, List[str]] = {}
-        self.processors: Dict[str, Any] = {}
+    def __init__(self, redis_url: str = "redis://localhost:6379/10") -> None:
+        self.redis_client = redis.StrictRedis.from_url(redis_url, decode_responses=True)
+        self.flows_key = "global_registry:flows"
+        self.triggers_key = "global_registry:triggers"
+        self.flow_processors_key = "global_registry:flow_processors"
+        self.processors_key = "global_registry:processors"
 
+    @property
+    def flows(self) -> Dict[str, Dict]:
+        flows = self.redis_client.hgetall(self.flows_key)
+        return {flow_id: json.loads(flow_data) for flow_id, flow_data in flows.items()} if flows else {}
+
+    @property
+    def triggers(self) -> Dict[str, str]:
+        return self.redis_client.hgetall(self.triggers_key)
+
+    @property
+    def processors(self) -> Dict[str, Callable]:
+        return self.redis_client.hgetall(self.processors_key)
+
+    @property
+    def flow_processors(self) -> Dict[str, List[str]]:
+        return self.redis_client.hgetall(self.flow_processors_key)
+
+    # Registry Management
     def load_flows(self) -> None:
-        validated_flows = []
+        self.clear_registry()
         for file_name in os.listdir(FLOW_DIR):
-            self.load_flow(file_name)    
-            validated_flows.append(file_name)
-
-        if not validated_flows:
-            return
-        
-        # self.clear_registry()
+            self.load_flow(file_name)
         self.register_global_processors()
 
+        with open("redis_store.json", "w") as file:
+            file.write(json.dumps(self.flows))
 
     def load_flow(self, flow_filename: str) -> None:
         from utils.validators import FlowValidator
@@ -62,118 +77,124 @@ class GlobalRegistry:
 
         try:
             file_path = os.path.join(FLOW_DIR, flow_filename)
-            with open(file_path, "r") as f:
-                flow = json.load(f)
-
+            flow = read_json_file(file_path)
             validation_error = validator.validate_flow(flow)
-            if validation_error:    
-                # self.invalidate_file(file_path)
-                return validation_error
+            if validation_error:
+                logger.error(f"Validation error for flow: {flow_filename}")
+                return
 
             self.register_flow(flow)
-            logger.info(f"Successfully loaded {flow.get('id')}")
-            return ""
-        
+            logger.info(f"Successfully loaded flow: {flow.get('id')}")
+
+        except json.JSONDecodeError as e:
+            logger.error(f"Invalid json file: {flow_filename}, Error: {e}")
         except Exception as e:
-            logger.error(f"Failed to validate flow: {flow_filename},  Error: {e}")
-            return f"Error: {e}"
+            logger.error(f"Failed to load flow: {flow_filename}, Error: {e}")
 
     def deactivate_flow(self, flow_id: str) -> str:
         flow_filename = f"{flow_id}.json"
         file_path = os.path.join(FLOW_DIR, flow_filename)
         archive_file_path = os.path.join(ARCHIVE_DIR, flow_filename)
-        
+
         if not os.path.exists(file_path):
-            if os.path.exists(archive_file_path):
+            if self.redis_client.hexists(self.flows_key, flow_id):
                 return f"Flow:{flow_id} is already inactive"
-            return f"Flow:{flow_id} does not exists, cannot deactivate."
-        
+            return f"Flow:{flow_id} does not exist, cannot deactivate."
+
         os.rename(file_path, archive_file_path)
         self.unregister_flow(flow_id)
-        logger.info(f"Successfully Deactivated {flow_id}")
+        logger.info(f"Successfully deactivated flow: {flow_id}")
         return ""
 
-    def update_flow(self, flow_id: str, updated_data: dict):
+    def update_flow(self, flow_id: str, updated_data: dict) -> str:
         from utils.validators import FlowValidator
         validator = FlowValidator()
 
         try:
-            flow_filename = f"{flow_id}.json"
-            file_path = os.path.join(FLOW_DIR, flow_filename)
-            current_flow_data = read_json_file(file_path)
+            current_flow_data = self.redis_client.hget(self.flows_key, flow_id)
+            if not current_flow_data:
+                return f"Flow with ID '{flow_id}' does not exist."
+
+            current_flow_data = json.loads(current_flow_data)
             updated_flow_data = {**current_flow_data, **updated_data}
 
             validation_error = validator.validate_flow(updated_flow_data)
-            if validation_error:    
+            if validation_error:
                 return validation_error
 
-            update_json_file(file_path, updated_flow_data)
+            self.redis_client.hset(self.flows_key, flow_id, json.dumps(updated_flow_data))
             self.register_flow(updated_flow_data)
-            logger.info(f"Successfully updated {flow_id}")
+            logger.info(f"Successfully updated flow with ID '{flow_id}'")
             return ""
-        
+
         except Exception as e:
-            logger.error(f"Failed to validate flow: {flow_filename},  Error: {e}")
+            logger.error(f"Failed to update flow with ID '{flow_id}': {e}")
             return f"Error: {e}"
-
-    def invalidate_file(self, file_path: str) -> None:
-        directory, file_name = os.path.split(file_path)
-        invalid_file_path = os.path.join(directory, f".{file_name}")
-        os.rename(file_path, invalid_file_path)
-        logger.error(f"File invalidated: {file_path}")
-
-    def register_flow_processors(self, flow_id: str) -> None:
-        flow_processor_script_path = os.path.join(PROCESSOR_DIR, f"{flow_id}.py")
-        functions = get_functions_from_script(flow_processor_script_path)
-        self.flow_processors[flow_id] = functions
-        logger.info(f"Registered flow: {flow_id} processors({len(functions)})")
-
-    def register_global_processors(self) -> None:
-        global_processors_script_path = os.path.join(PROCESSOR_DIR, "processors.py")
-        functions = get_functions_from_script(global_processors_script_path)
-        self.processors = functions
-        logger.info(f"Registered global processors({len(functions)})")
 
     def register_flow(self, flow: Dict) -> None:
         flow_id = flow["id"]
-        self.flows[flow_id] = flow
         steps = flow.get("steps", [])
-        steps_by_id = {step["id"]: step for step in steps}
-        flow["steps_by_id"] = steps_by_id
+        flow["steps_by_id"] = {step["id"]: step for step in steps}
 
-        self.triggers[flow.get("trigger", "")] = flow_id
+        self.redis_client.hset(self.flows_key, flow_id, json.dumps(flow))
+        self.redis_client.hset(self.triggers_key, flow["trigger"], flow_id)
+
         self.register_flow_processors(flow_id)
         logger.info(f"Flow '{flow_id}' registered.")
 
     def unregister_flow(self, flow_id: str) -> None:
-        self.flows.pop(flow_id, None)
-        self.triggers.pop(flow_id, None)
-        self.processors.pop(flow_id, None)
-        self.flow_processors.pop(flow_id, None)
+        self.redis_client.hdel(self.flows_key, flow_id)
+        self.redis_client.hdel(self.triggers_key, flow_id)
+        self.redis_client.hdel(self.flow_processors_key, flow_id)
         logger.info(f"Flow '{flow_id}' unregistered successfully.")
 
+    def register_global_processors(self) -> None:
+        global_processors_script_path = os.path.join(PROCESSOR_DIR, "processors.py")
+        functions = get_functions_from_script(global_processors_script_path)
+        function_references = {func_name: f"{function.__module__}.{func_name}" for func_name, function in functions.items()}
+        self.redis_client.hmset(self.processors_key, function_references)
+        logger.info(f"Registered global processors ({len(function_references)})")
+
+    def register_flow_processors(self, flow_id: str) -> None:
+        flow_processor_script_path = os.path.join(PROCESSOR_DIR, f"{flow_id}.py")
+        functions = get_functions_from_script(flow_processor_script_path)
+        function_references = {func_name: f"{function.__module__}.{func_name}" for func_name, function in functions.items()}
+        self.redis_client.hset(self.flow_processors_key, flow_id, json.dumps(function_references))
+        logger.info(f"Registered processors for flow: {flow_id}")
+
     def clear_registry(self) -> None:
-        self.flows.clear()
-        self.triggers.clear()
-        self.flow_processors.clear()
-        self.processors.clear()
+        self.redis_client.delete(self.flows_key)
+        self.redis_client.delete(self.triggers_key)
+        self.redis_client.delete(self.flow_processors_key)
+        self.redis_client.delete(self.processors_key)
         logger.info("Global registry cleared.")
 
-    def get_step_by_id(self, flow_id: str, step_id: str) -> Optional[Dict]:
-        flow = self.flows.get(flow_id, {})
-        step = flow.get("steps_by_id", {}).get(step_id, None)
-        return step
+    def get_step_by_id(self, flow_id: str, step_id: str) -> Dict:
+        flow = self.redis_client.hget(self.flows_key, flow_id)
+        flow = json.loads(flow) if flow else {}
 
-    def get_flow_by_trigger(self, trigger: str) -> Optional[Dict]:
-        flow_id = self.triggers.get(trigger)
-        return self.flows.get(flow_id) if flow_id else None
+        return flow.get("steps_by_id", {}).get(step_id, {})
 
-    def get_processors_for_flow(self, flow_id: str) -> Optional[List[str]]:
-        return self.flow_processors.get(flow_id)
+    def get_flow_by_trigger(self, trigger: str) -> Dict:
+        flow_id = self.redis_client.hget(self.triggers_key, trigger)
+        flow = self.redis_client.hget(self.flows_key, flow_id) if flow_id else ""
+        return json.loads(flow) if flow else {}
 
-    def get_processor_by_name(self, name: str) -> Optional[Any]:
-        return self.processors.get(name)
+    def get_processors_for_flow(self, flow_id: str) -> List[str]:
+        flow_processors = self.redis_client.hget(self.flow_processors_key, flow_id)
+        return json.loads(flow_processors) if flow_processors else []
 
-    def refresh_registry(self, flow_dir: str) -> None:
-        self.load_flows(flow_dir)
+    def get_processor_by_name(self, name: str) -> Callable:
+        processor_path = self.redis_client.hget(self.processors_key, name)
+        if not processor_path:
+            return None
+        
+        print("Processor path", processor_path)
+        module_name, func_name = processor_path.rsplit(".", 1)
+        module = importlib.import_module(module_name)
+        processor_function = getattr(module, func_name)
+        return processor_function
+
+    def refresh_registry(self) -> None:
+        self.load_flows()
         logger.info("Global registry refreshed.")
